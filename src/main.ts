d73adeb230d2f8e98b83456e5063c65694f29b74
kylin <liux1118@qq.com>
2022-08-15 20:58:28 +0800

    feat: 全局异常捕获

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // 替换使用Fastify
-import { VersioningType } from '@nestjs/common';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 
 import { NestFactory } from '@nestjs/core';
 
@@ -10,6 +10,10 @@ import {
 
 import { AppModule } from './app.module';
 
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -18,8 +22,17 @@ async function bootstrap() {
 
   // 接口版本化管理
   app.enableVersioning({
+    // 全局配置请求配置
+    // defaultVersion: '1',
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
+
+  // 统一响应体格式 useGlobalInterceptors 全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器 useGlobalFilters 全局异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
   await app.listen(3000, () => {
     console.log(`项目运行在http://localhost:3000`);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,19 +7,40 @@ import {
   Param,
   Delete,
   Version,
+  VERSION_NEUTRAL,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+
+import { BusinessException } from 'src/common/exceptions/business.exception';
 
 /**
  * Controller是一个装饰器
  * 'user'相当于路由前缀
  * Version是版本号装饰器，使用了之后需要携带版本号才能正常进行Http请求
  */
-@Controller('user')
+
+// 对整个Controller进行版本控制
+@Controller({
+  path: 'user',
+  version: '1',
+})
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  // 伪造业务异常场景
+  @Get('findBusinessError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findBusinessError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('你这个参数错了');
+    }
+    return this.userService.findAll();
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -27,7 +48,6 @@ export class UserController {
   }
 
   @Get()
-  @Version('1')
   findAll() {
     return this.userService.findAll();
   }
