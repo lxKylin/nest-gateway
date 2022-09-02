86b23a7f28bf993232741cfe38a34115bb282896
kylin <liux1118@qq.com>
2022-09-02 16:40:38 +0800

    feat: 自定义日志

--- src/main.ts
+++ src/main.ts
@@ -12,20 +12,27 @@ import {
   NestFastifyApplication
 } from '@nestjs/platform-fastify';
 
+import fastify from 'fastify';
+
 import { AppModule } from './app.module';
 
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { FastifyLogger } from './common/logger';
 
 import { generateDocument } from './doc';
 
 declare const module: any;
 
 async function bootstrap() {
+  const fastifyInstance = fastify({
+    logger: FastifyLogger
+  });
+  // 为了框架的性能使用 Fastify 来替换底层框架之后，需要使用下述代码来开启 Fastify 的日志系统：
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(fastifyInstance)
   );
 
   // 设置全局路由前缀
@@ -69,7 +76,8 @@ bootstrap();
 // import { AppModule } from './app.module';
 
 // async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
+// 普通开启日志
+//   const app = await NestFactory.create(ApplicationModule, { logger: true });
 //   await app.listen(3000, () => {
 //     console.log(`项目运行在http://localhost:3000`);
 //   });
